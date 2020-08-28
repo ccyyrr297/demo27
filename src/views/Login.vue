803492b97f496838ee6e5c5c3c856776af9bcf02
墨尘 <389079622@qq.com>
2020-08-28 10:06:37 +0800

    安装组件插件图标样式

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,5 +1,9 @@
 <template>
-  <div>我是登录组件</div>
+  <div>
+    我是登录组件
+    <span class="iconfont icondianzan"></span>
+    <span class="iconfont iconweixin"></span>
+  </div>
 </template>
 
 <script>
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,10 +1,16 @@
 <template>
-  <div>我是注册组件</div>
+  <div>
+    我是注册组件
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+  </div>
 </template>
 
 <script>
 export default {}
 </script>
 
-<style>
-</style>
+<style></style>
